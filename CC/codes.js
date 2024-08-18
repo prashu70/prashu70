13e4b15b107caf20112deca5b874935556ebb006
Prasanth Kumar <153743758+prashu70@users.noreply.github.com>
2024-08-18 18:58:49 +0530

    Add files via upload

---
+++ CC/codes.js
@@ -0,0 +1,17 @@
+const countryList = {
+    AED: "AED",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    INR: "IN"
+};
